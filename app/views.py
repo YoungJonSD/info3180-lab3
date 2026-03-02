86c1b5cc703a76e660193e7286b1baffde154068
Stephen-Jon Dixon <stephenjondixon1@gmail.com>
2026-03-02 15:48:07 -0500

    Implemented Contact page functionality

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -15,8 +18,23 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Stephen-Jon Dixon")
 
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    """Render the website's contact page."""
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(
+            subject=form.subject.data,
+            sender=form.email.data,
+            recipients=['stephenjondixon1@gmail.com'],
+            body=f'From: {form.name.data} <{form.email.data}>\n\n{form.message.data}'
+        )
+        mail.send(msg)
+        flash("Form submitted successfully.", "success")
+        return redirect(url_for('home'))
+    return render_template('contact.html', form=form)
 
 ###
 # The functions below should be applicable to all Flask apps.
